e12695652d92afaf62cb582c9385ca67cfb5b653
Julien Auger <julien.auger37@laposte.net>
2023-08-21 02:17:05 +0200

    Fixing error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import Page from '../components/layout/Page';
-import LinkedButton from '../components/utils/LinkedButton';
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   return (
-    <div className="bg-neutral-950 w-full min-h-screen">
-      <Page className="flex items-center justify-center w-full flex-col">
-        <h2 className="text-5xl font-bold text-white mb-8 max-w-2xl text-center">
+    <div className="bg-bg-blue flex w-full min-h-screen items-center justify-center">
+      <div className="flex items-center  w-full flex-col ">
+        <h2 className="text-3xl font-semibold text-white  max-w-2xl text-center">
           Oups ! Il semblerait que la page recherchée n&apos;existe plus
         </h2>
-        <LinkedButton path="/">Retourner à l&apos;accueil</LinkedButton>
-      </Page>
+        <Link className="relative" to="/">
+          <span className="bg-accent-blue rounded-lg p-4 text-sm text-white transition-all hover:bg-accent-blue/75">
+            Retourner à l&apos;accueil
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
