1af9b4baf4baa4f47776b32e7d541cdf20d69852
Julien Auger <jauger@amiltone.com>
2023-10-15 16:50:15 +0200

    Adding sitemap vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [react(), mdx()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://guild-o-matic.fr',
+    }),
+    mdx(),
+  ],
 });
